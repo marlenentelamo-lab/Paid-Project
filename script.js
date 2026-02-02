b467b1e38782006895bc46dab3d19d6eeb6b16f5
marlenentelamo-lab <marlene.ntelamo@gmail.com>
2026-02-02 17:57:42 +0200

    All portfolio code

---
+++ script.js
@@ -0,0 +1,18 @@
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear();
+
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+
+  alert(`Thank you, ${name}! Your message has been sent.`);
+
+  form.reset();
+});
